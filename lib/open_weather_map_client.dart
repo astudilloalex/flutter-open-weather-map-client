524ab6ac7e494639854246c7ae26a9e0c3cb2e14
astudilloalex <astudilloalex1@gmail.com>
2021-08-02 19:19:44 -0500

    Export required dart files

--- lib/open_weather_map_client.dart
+++ lib/open_weather_map_client.dart
@@ -1,5 +1,8 @@
 library open_weather_map_client;
 
-export 'src/constants.dart';
+export 'src/common/constants.dart';
+export 'src/model/city.dart';
+export 'src/model/country.dart';
 export 'src/model/weather.dart';
 export 'src/open_weather_map.dart';
+export 'src/open_weather_map_provider.dart';
