271dc28da9b2e341369e4243d1ac0af7e73fb4d5
astudilloalex <astudilloalex1@gmail.com>
2021-08-09 09:01:26 -0500

    Remove test

--- test/open_weather_map_client_test.dart
+++ test/open_weather_map_client_test.dart
@@ -1,13 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:open_weather_map_client/open_weather_map_client.dart';
-
-void main() {
-  final OpenWeatherMap _api = OpenWeatherMap(
-    apiKey: '856822fd8e22db5e1ba48c0e7d69844a',
-  );
-  test('Check values from Weather class', () async {
-    //final Weather weather = _api.currentWeather;
-    //expect(weather.humidity, isA<int>());
-  });
-}
+void main() {}
