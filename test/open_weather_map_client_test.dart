265460186b02c2e97aca6afed31538b88077a3e9
Alex Astudillo <astudilloalex1@gmail.com>
2021-10-25 16:31:32 -0500

    Update test --no important changes

--- test/open_weather_map_client_test.dart
+++ test/open_weather_map_client_test.dart
@@ -4,7 +4,7 @@ import 'current_weather_test.dart';
 void main() {
   // OpenWeatherMap instance with api key.
   const OpenWeatherMap weatherMap = OpenWeatherMap(
-    apiKey: '6d085bec102ae019e74d51e28370ad59',
+    apiKey: '21450ca9db82aa862173bf64669fe6a6',
   );
   const CurrentWeatherTest currentWeatherTest = CurrentWeatherTest(
     weatherMap: weatherMap,
